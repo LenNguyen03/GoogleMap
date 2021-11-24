32be25a237853184d14690a23056ca6770466c9e
LenNguyenVan13 <nguyenvanlentech@gmail.com>
2021-11-24 23:01:47 +0700

    Initial commit

---
+++ app/src/main/java/com/se/googlemap/GoogleMapCustom.java
@@ -0,0 +1,72 @@
+package com.se.googlemap;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.widget.SearchView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.se.googlemap.databinding.ActivityMapsBinding;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.util.List;
+
+public class GoogleMapCustom extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    SupportMapFragment mapFragment;
+    SearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_map_custom);
+        searchView = findViewById(R.id.sv_location);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if(location != null || !location.equals("")){
+                    Geocoder geocoder = new Geocoder(GoogleMapCustom.this);
+                    try{
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+}
